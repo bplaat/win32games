1eedfe02c1b03e3bca469edddab8bb835416e23c
Bastiaan van der Plaat <bplaat@users.noreply.github.com>
2017-01-09 13:39:43 +0100

    Add files via upload

--- plaatquest.c
+++ plaatquest.c
@@ -11,6 +11,7 @@ double random () { double x = sin(seed++) * 1e4; return x - floor(x); }
 int rand (min, max) { return floor(random() * (max - min + 1)) + min; }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+  PAINTSTRUCT ps;
   switch (msg) {
     case WM_CREATE:
       seed = getpid(), turn = 1, turn_str = (char*)malloc(13), ws_str = (char*)malloc(15), map = (char*)malloc(MW * MH);
@@ -27,32 +28,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
       }}
       for (i = 0; i < MW; i++) for (j = 0; j < MH-x%2; j++) if (*(map+i*MH+j) == 1) world_size++;
     break;
-    case WM_PAINT: {
-      PAINTSTRUCT ps;
-      HDC hdc = BeginPaint(hwnd, &ps);
-      SelectObject(hdc, CreateFont(20, 0, 0, 0, 400, 0, 0, 0, 0, 0, 0, 0, 0, "Arial"));
-      SetTextColor(hdc, RGB(255, 255, 255));
-      SetBkColor(hdc, TRANSPARENT);
+    case WM_PAINT:
+      BeginPaint(hwnd, &ps);
+      SelectObject(ps.hdc, CreateFont(20, 0, 0, 0, 400, 0, 0, 0, 0, 0, 0, 0, 0, "Arial"));
+      SetTextColor(ps.hdc, RGB(255, 255, 255));
+      SetBkColor(ps.hdc, TRANSPARENT);
       
-      SelectObject(hdc, CreateSolidBrush(RGB(0, 0, 175)));
-      Rectangle(hdc, 0, 0, W, H);
+      SelectObject(ps.hdc, CreateSolidBrush(RGB(0, 0, 175)));
+      Rectangle(ps.hdc, 0, 0, W, H);
       
       for (i = 0; i < MW; i++) for (j = 0; j < MH-i%2; j++) {
-        if (*(map+i*MH+j) == 0) SelectObject(hdc, CreateSolidBrush(RGB(0, 0, 175)));
-        if (*(map+i*MH+j) == 1) SelectObject(hdc, CreateSolidBrush(RGB(0, 175, 0)));
-        if (*(map+i*MH+j) == 2) SelectObject(hdc, CreateSolidBrush(RGB(255, 255, 255)));
+        if (*(map+i*MH+j) == 0) SelectObject(ps.hdc, CreateSolidBrush(RGB(0, 0, 175)));
+        if (*(map+i*MH+j) == 1) SelectObject(ps.hdc, CreateSolidBrush(RGB(0, 175, 0)));
+        if (*(map+i*MH+j) == 2) SelectObject(ps.hdc, CreateSolidBrush(RGB(255, 255, 255)));
         x = i * 30, y = j * 40 + i % 2 * 20;
         POINT points[6] = {{x, y+20},{x+10, y},{x+30, y},{x+40, y+20},{x+30, y+40},{x+10, y+40}};
-        Polygon(hdc, points, 6);
+        Polygon(ps.hdc, points, 6);
       }
       
       sprintf(&turn_str, "Turn: %6d", turn);
-      TextOut(hdc, 20, H - 60, (LPCTSTR)&turn_str, 13);
+      TextOut(ps.hdc, 20, H - 60, (LPCTSTR)&turn_str, 13);
       sprintf(&ws_str, "World size: %3d", world_size);
-      TextOut(hdc, 20, H - 40, (LPCTSTR)&ws_str, 15);
+      TextOut(ps.hdc, 20, H - 40, (LPCTSTR)&ws_str, 15);
       EndPaint(hwnd, &ps);
-    } break;
-    case WM_CLOSE: DestroyWindow(hwnd); break;
+    break;
     case WM_DESTROY: PostQuitMessage(0); break;
     default: return DefWindowProc(hwnd, msg, wParam, lParam);
   }
@@ -60,10 +59,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
   WNDCLASS wc = {0}; wc.lpszClassName = "PlaatQuest", wc.lpfnWndProc = WndProc, wc.hInstance = hInstance;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW); RegisterClass(&wc);
-  HWND hwnd = CreateWindow(wc.lpszClassName, wc.lpszClassName, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^
-  WS_MAXIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, W, H, NULL, NULL, hInstance, NULL);
-  ShowWindow(hwnd, nCmdShow);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW), wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 175)); RegisterClass(&wc);
+  HWND hwnd = CreateWindow(wc.lpszClassName, wc.lpszClassName, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+  CW_USEDEFAULT, CW_USEDEFAULT, W, H, NULL, NULL, hInstance, NULL); ShowWindow(hwnd, nCmdShow);
   MSG msg; while(GetMessage(&msg, NULL, 0, 0)) TranslateMessage(&msg), DispatchMessage(&msg);
   return msg.wParam;
 }
